08d7f3689d80232df86f03714402550a57c994e1
Knudn <kor@kroland.no>
2024-01-17 14:16:46 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-source ./fastapi_app_env/bin/activate
-
 x11vnc -display :0 -wait 50 -noxdamage -auth guess -viewonly -forever &
 ./vnc/novnc_proxy &
 
 if [ ! -d fastapi_app_env ]; then
   ./install.sh
 fi
+
+source ./fastapi_app_env/bin/activate
 
 python main.py
