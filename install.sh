c453dac6ab8677635ec3eeeebb30daff714376fe
Knudn <kor@kroland.no>
2024-01-19 23:17:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo apt-get update
 
 # Install Python 3 and pip
-sudo apt-get install -y python3 python3-pip python3-virtualenv screen firefox-esr
+sudo apt-get install -y python3 python3-pip python3-virtualenv screen firefox-esr x11vnc
 
 # Create a virtual environment for the application
 virtualenv fastapi_app_env
